b0646f767a1dc08484fab7a5b3b11f0074812c81
marcoferre <10467480@polimi.it>
2020-08-27 10:12:17 +0200

    Controller -fix UpperCase char bug

--- type-to-the-rhythm-client/assets/scripts/controller.js
+++ type-to-the-rhythm-client/assets/scripts/controller.js
@@ -8,11 +8,12 @@ var db
 var message
 var trackSteps, steps, bar = 0
 const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`˜!.@#$%^&*()-_=+;\\|{}[] \":/?><".split("")
-const pitch = [-24, null, null, null, -17, null, -15, null, -13, -21, null, -19, -22, -5, -9, null, -7, -1, null, null, -3, null, null, null, -12]
-const triggerK = [1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0]
-const triggerS = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1]
-const triggerH = [0, 1, 2, 1, 0, 0, 2, 0, 0, 0, 2, 0, 1, 0, 0, 1, 1, 0, 2, 1, 0, 0, 2, 2, 0, 1]
-const triggerP = [0, 0, 1, 0, 2, 1, 3, 0, 0, 3, 0, 5, 4, 4, 0, 0, 1, 5, 0, 0, 0, 2, 2, 0, 2, 0]
+const pitch = [-24, null, null, null, -17, null, -15, null, -13, -21, null, -19, -22, -5, -9, null, -7, -1, null, null, -3, null, null, null, -12, -24, null, null, null, -17, null, -15, null, -13, -21, null, -19, -22, -5, -9, null, -7, -1, null, null, -3, null, null, null, -12]
+//                a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z
+const triggerK = [1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0]
+const triggerS = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1]
+const triggerH = [0, 1, 2, 1, 0, 0, 2, 0, 0, 0, 2, 0, 1, 0, 0, 1, 1, 0, 2, 1, 0, 0, 2, 2, 0, 1, 0, 1, 2, 1, 0, 0, 2, 0, 0, 0, 2, 0, 1, 0, 0, 1, 1, 0, 2, 1, 0, 0, 2, 2, 0, 1]
+const triggerP = [0, 0, 1, 0, 2, 1, 3, 0, 0, 3, 0, 5, 4, 4, 0, 0, 1, 5, 0, 0, 0, 2, 2, 0, 2, 0, 0, 0, 1, 0, 2, 1, 3, 0, 0, 3, 0, 5, 4, 4, 0, 0, 1, 5, 0, 0, 0, 2, 2, 0, 2, 0]
 
 btnstart.onclick = start
 
@@ -45,9 +46,9 @@ function getMessages() {
     db.collection("messages").where("read", "==", "N").orderBy("datetime", "desc").get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
             messageStack.push(doc.data())
-            //db.collection('messages').doc(doc.id).update({
-            //    read: "Y"
-            //});
+            db.collection('messages').doc(doc.id).update({
+                read: "Y"
+            });
         });
     });
     console.log(messageStack)
@@ -82,9 +83,9 @@ function start() {
             steps[i] = (steps[i] + 1) % message[i].length;
         }
 
-        if (trackSteps[0].innerText !== "") playBass(getFrequency(pitch[alphabet.indexOf(trackSteps[0].innerText)])/4, 1)
-        if (trackSteps[4].innerText !== "") playPad(getFrequency(pitch[alphabet.indexOf(trackSteps[4].innerText)])/2, 1)
-        if (trackSteps[5].innerText !== "") playLead(getFrequency(pitch[alphabet.indexOf(trackSteps[5].innerText)]), 1)
+        if (trackSteps[0].innerText !== "" && pitch[alphabet.indexOf(trackSteps[0].innerText)] != null) playBass(getFrequency(pitch[alphabet.indexOf(trackSteps[0].innerText)])/4, 1)
+        if (trackSteps[4].innerText !== "" && pitch[alphabet.indexOf(trackSteps[4].innerText)] != null) playPad(getFrequency(pitch[alphabet.indexOf(trackSteps[4].innerText)])/2, 0.5)
+        if (trackSteps[5].innerText !== "" && pitch[alphabet.indexOf(trackSteps[5].innerText)] != null) playLead(getFrequency(pitch[alphabet.indexOf(trackSteps[5].innerText)]), 0.5)
         if (triggerK[alphabet.indexOf(trackSteps[1].innerText)] === 1 && trackSteps[1].innerText !== "") playSample("short-kick")
         if (triggerS[alphabet.indexOf(trackSteps[2].innerText)] === 1 && trackSteps[2].innerText !== "") playSample("snare")
         if (triggerH[alphabet.indexOf(trackSteps[3].innerText)] === 1 && trackSteps[3].innerText !== "") playSample("close-hihat")
